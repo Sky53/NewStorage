51b08e6f17af2b0db001799d9df208f28a5f4795
Sky53 <45273067+Sky53@users.noreply.github.com>
2020-01-15 16:04:24 +0300

    def

---
+++ NewStorage/NewStorage/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NewStorage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
