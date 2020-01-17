089c3585ce8c18f817553f72b5ba0c2b0758546d
Sky53 <45273067+Sky53@users.noreply.github.com>
2020-01-17 12:55:54 +0300

    add colum UserId in Product

--- NewStorage/Storage.Domain/Order.cs
+++ NewStorage/Storage.Domain/Order.cs
@@ -9,6 +9,7 @@ namespace Storage.Domain
     {
         [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Number { get; set; }
+        public Guid UserId { get; set; }
         public User User { get; set; }
         public List<OrderProduct> OrderProduct { get; set; }
         public DateTime CreatedDate { get; set; }
