a75f6fc665fc1352601dfe465335fb04e4ff7518
Sky53 <45273067+Sky53@users.noreply.github.com>
2020-01-19 18:56:09 +0300

    test

---
+++ NewStorage/Storage.API/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Storage.Business;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Storage.API.Controllers
+{
+    [ApiController]
+    //serv/api/
+    [Route("api/[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpPost]
+        public void AddProduct(string name, decimal price)
+        {
+            //todo
+        }
+    }
+}
---
+++ NewStorage/Storage.DAL/Repository/IProductRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Storage.DAL.Repository
+{
+    interface IProductRepository
+    {
+    }
+}
