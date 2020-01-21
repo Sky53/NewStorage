d480dad5486d98c737153cecf31f37582b321275
Sky53 <45273067+Sky53@users.noreply.github.com>
2020-01-21 16:46:49 +0300

    add anot

--- NewStorage/Storage.Domain/User.cs
+++ NewStorage/Storage.Domain/User.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Storage.Domain
 {
     public class User
     {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public List<Order> Orders { get; set; }
         public string Name { get; set; }
