a62a9577622c872c3854426aca9d5cfb2835f370
Sky53 <45273067+Sky53@users.noreply.github.com>
2020-01-21 16:48:42 +0300

    User ref

--- NewStorage/Storage.Domain/User.cs
+++ NewStorage/Storage.Domain/User.cs
@@ -9,11 +9,12 @@ namespace Storage.Domain
     {
         [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
-        public List<Order> Orders { get; set; }
         public string Name { get; set; }
         public string Login { get; set; }
         public string Password { get; set; }
         public DateTime Birthday { get; set; }
         public bool IsDeleted { get; set; }
+
+        public List<Order> Orders { get; set; }
     }
 }
