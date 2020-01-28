ae69de64cbc2f86b9ff70874bba340f91bb674fa
Sky53 <Sanyaga3@yandex.ru>
2020-01-28 18:13:18 +0300

    add costome

---
+++ NewStorage/Storage.DAL/Exception/StorageException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Storage.DAL.Exception
+{
+    [Serializable]
+    class StorageException : System.Exception // По какой-то магической причине иначе Exception он не видит
+    {
+        public StorageException()
+        {
+
+        }
+
+        public StorageException(string code) : base(code)
+        {
+
+        }
+    }
+}
